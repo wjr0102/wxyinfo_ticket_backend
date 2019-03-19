5bb33d24dc7edbeab5ab9c7e2850d9f49098980c
Wu <445048962@qq.com>
2019-03-20 00:59:47 +0800

    新增单元测试用例

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,7 +3,7 @@
 # @Author: Jingrou Wu
 # @Date:   2019-03-18 19:55:02
 # @Last Modified by:   Jingrou Wu
-# @Last Modified time: 2019-03-18 22:03:45
+# @Last Modified time: 2019-03-20 00:53:47
 
 """
 
@@ -24,7 +24,7 @@ import unittest
 
 from backend import app, db
 from backend.model import User, Seat
-from backend.model import queryTicketInfoByIdNumber, queryTicketInfoByName
+from backend.model import queryTicketInfoByIdNumber, queryTicketInfoByName, querySeatsByTimeSpan, querySeatsByName, querySeatsByIdNumber
 from random import choice
 
 
@@ -44,7 +44,7 @@ class DbTestCase(unittest.TestCase):
         db.create_all()
 
     def test_ticket_by_name(self):
-        """测试接口queryTicketInfoByIdNumber"""
+        """ 测试接口queryTicketInfoByName """
 
         users = User.query.all()
         user = choice(users)
@@ -55,8 +55,8 @@ class DbTestCase(unittest.TestCase):
         result = queryTicketInfoByName(u'无')
         self.assertIsNone(result)
 
-    def test_ticket_bt_idNumber(self):
-        """测试接口queryTicketInfoByName"""
+    def test_ticket_by_idNumber(self):
+        """ 测试接口queryTicketInfoByidNumber """
 
         users = User.query.all()
         user = choice(users)
@@ -67,8 +67,42 @@ class DbTestCase(unittest.TestCase):
         result = queryTicketInfoByName(u'无')
         self.assertIsNone(result)
 
+    def test_seat_by_timespan(self):
+        """ 测试接口querySeatsByTimeSpan """
+
+        users = User.query.all()
+        user = choice(users)
+        print(user.seats_time)
+        result = querySeatsByTimeSpan(user.seats_time, user.seats_time)
+        print(result)
+        self.assertIsNotNone(result)
+        result = querySeatsByTimeSpan('199912120300', '199912121203')
+        self.assertIsNone(result)
+
+    def test_seat_by_idNumber(self):
+        """ 测试接口querySeatsByIdNumber """
+
+        result = querySeatsByIdNumber('110105194502177537')     # 存在抽票结果
+        print(result)
+        self.assertIsNotNone(result)
+        result = querySeatsByIdNumber('440229199804084710')    # 不存在抽票结果
+        self.assertIsNone(result)
+        result = querySeatsByIdNumber('0')     # 不存在该用户
+        self.assertIsNone(result)
+
+    def test_seat_by_name(self):
+        """ 测试接口querySeatsByName """
+
+        result = querySeatsByName(u'宋桂芝')     # 存在抽票结果
+        print(result)
+        self.assertIsNotNone(result)
+        result = querySeatsByName(u'黄峰')    # 不存在抽票结果
+        self.assertIsNone(result)
+        result = querySeatsByName(u'0')     # 不存在该用户
+        self.assertIsNone(result)
+
     def tearDown(self):
-        """为测试方便，暂时忽视tear down函数功能"""
+        """ 为测试方便，暂时忽视tear down函数功能 """
         # db.session.remove()
         # db.drop_all()
         pass
